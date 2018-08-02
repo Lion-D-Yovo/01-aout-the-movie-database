c62a80d6244bf8e569b1564635a817dba9f354ea
Lion-D-Yovo <lion.abonnements@gmail.com>
2018-08-02 15:09:56 +0200

    commit 03

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
+movie = Movie.create( title: 'Willow', release_year: 1988, director: 'Ron Howard' )
+movie = Movie.create( title: 'E.T.', release_year: 1982, director: 'Steven Spielberg' )
+movie = Movie.create(title: 'Les Goonies', release_year: 1985, director: 'Richard Donner' )
+movie = Movie.create( title: 'Rambo', release_year: 1983, director: 'Ted Kotcheff' )
+movie = Movie.create(title: 'SOS fantomes', release_year: 1984, director: 'Ivan Reitman' )
