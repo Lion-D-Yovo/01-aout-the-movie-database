b5b1791890988dc565c935cccbc5cded2c21e0cf
Lion-D-Yovo <lion.abonnements@gmail.com>
2018-08-02 17:23:21 +0200

    commit 6

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Director.destroy_all
 Movie.destroy_all
 
-movie = Movie.create( title: 'Willow', release_year: 1988, director: 'Ron Howard' )
-movie = Movie.create( title: 'E.T.', release_year: 1982, director: 'Steven Spielberg' )
-movie = Movie.create(title: 'Les Goonies', release_year: 1985, director: 'Richard Donner' )
-movie = Movie.create( title: 'Rambo', release_year: 1983, director: 'Ted Kotcheff' )
-movie = Movie.create(title: 'SOS fantomes', release_year: 1984, director: 'Ivan Reitman' )
+Ron_H= Director.create( first_name: 'Ron', last_name: 'Howard' )
+Steven_S = Director.create( first_name: 'Steven', last_name: 'Spielberg' )
+Richard_D = Director.create( first_name: 'Richard', last_name: 'Donner' )
+Ted_K= Director.create( first_name: 'Ted', last_name: 'Kotcheff' )
+Ivan_R = Director.create( first_name: 'Ivan', last_name: 'Reitman' )
+
+
+movie = Movie.create( title: 'Rambo', release_year: 1983, director_id: Ted_K.id  )
+movie = Movie.create(title: 'SOS fantomes', release_year: 1984, director_id: Ivan_R.id )
+movie = Movie.create( title: 'Willow', release_year: 1988, director_id: Ron_H.id )
+movie = Movie.create( title: 'E.T.', release_year: 1982, director_id: Steven_S.id )
+movie = Movie.create(title: 'Les Goonies', release_year: 1985, director_id: Richard_D.id )
